df358761658a589c81da36a6fcb135a9c77cc3c6
devmailar <veex1233@gmail.com>
2022-01-29 12:26:01 +0200

    add files

---
+++ index.js
@@ -0,0 +1,10 @@
+let bod = document.querySelector("body")
+
+window.addEventListener('load', (event) => {
+  // for (let i = 1; i < 50; i++) {
+  //   setTimeout(function timer() {
+     bod.classList.toggle("dark")
+     bod.classList.toggle("transition")
+  //   }, i * 4000);
+  // }
+})
