ebc9cdee42269c54fa342c83dc3212ceb51ad0ff
devmailar <83369389+devmailar@users.noreply.github.com>
2022-03-04 15:02:14 +0200

    Update post.js

--- assets/js/post.js
+++ assets/js/post.js
@@ -29,8 +29,8 @@ function sendMessage() {
     else {
       request.open("POST", "https://discord.com/api/webhooks/942399439362990090/nL_f2NZr1td3y_AMzww-D467TbaEB2WPuD72GjUAo5ilIClR588QEvyuGIiI_V35O9oQ");
       request.setRequestHeader("Content-Type", "application/json")
-      header("Access-Control-Allow-Origin: *");
-      
+      request.header("Access-Control-Allow-Origin: *");
+
       let myEmbed = {
         author: {
           name: "New Contact Form Submission"
