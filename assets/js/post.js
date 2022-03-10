95313ea20e428e9dcb1513a536bbd6cc134f7068
devmailar <83369389+devmailar@users.noreply.github.com>
2022-03-10 17:55:56 +0200

    Update post.js

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,7 +1,3 @@
-function refreshPage() {
-  window.location.reload();
-}
-
 function sendMessage() {
   let name = document.querySelector(".name").value;
   let email = document.querySelector(".email").value;
@@ -12,42 +8,30 @@ function sendMessage() {
   let webhookName = "Mailar";
   let color = "#ff0000";
 
-  if (message.length < 1 || name.length < 1 || subject.length < 1 || email.length < 1) {
-    alert("Textbox is empty!");
-  } else if (message.length > 300 || name.length > 20 || subject.length > 20 || email.length > 20) {
-    alert(` 
-      Error on sending message!
-      message is too long!
-      characters: ${message.length}
-    `);
-    
-  } else {
+  const request = new XMLHttpRequest();
+  request.open("POST", "https://discord.com/api/webhooks/951506034957123614/gO6PRpMBDnSAhZOzm-04cKr7iA7mMHBcpPsG_iqF8QOjLLPS8PdOxTJn04BW5vv34082");
+  request.setRequestHeader("Content-Type", "application/json");
 
-    const request = new XMLHttpRequest();
-    request.open("POST", "https://discord.com/api/webhooks/951506034957123614/gO6PRpMBDnSAhZOzm-04cKr7iA7mMHBcpPsG_iqF8QOjLLPS8PdOxTJn04BW5vv34082");
-    request.setRequestHeader('Content-Type', 'application/json');
+  let myEmbed = {
+    author: {
+      name: "New Contact Form Submission",
+    },
 
-    let myEmbed = {
-      author: {
-        name: "New Contact Form Submission",
-      },
+    title: "Subject: " + subject,
+    description:
+      "Name: " + name + "\nEmail: " + email + "\nSubject: " + subject + "\nMessage: " + message + "\n\nSended at **" + time + "** \n" + currentDate,
+    color: hexToDecimal(color),
+  };
 
-      title: "Subject: " + subject,
-      description:
-        "Name: " + name + "\nEmail: " + email + "\nSubject: " + subject + "\nMessage: " + message + "\n\nSended at **" + time + "** \n" + currentDate,
-      color: hexToDecimal(color),
-    };
+  const params = {
+    username: webhookName,
+    avatar_url: "",
+    embeds: [myEmbed],
+  };
 
-    const params = {
-      username: webhookName,
-      avatar_url: "",
-      embeds: [myEmbed],
-    };
+  request.send(JSON.stringify(params));
 
-    request.send(JSON.stringify(params));
-
-    function hexToDecimal(hex) {
-      return parseInt(hex.replace("#", ""), 16);
-    }
+  function hexToDecimal(hex) {
+    return parseInt(hex.replace("#", ""), 16);
   }
 }
