d30eecdb170fb9778c97888524d0b0eed1d88dbf
devmailar <83369389+devmailar@users.noreply.github.com>
2022-03-10 18:11:35 +0200

    try

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,4 +1,5 @@
 function sendMessage() {
+  console.log("sending")
   const request = new XMLHttpRequest();
   request.open("POST", "https://discord.com/api/webhooks/951506034957123614/gO6PRpMBDnSAhZOzm-04cKr7iA7mMHBcpPsG_iqF8QOjLLPS8PdOxTJn04BW5vv34082");
   request.setRequestHeader("Content-type", "application/json");
