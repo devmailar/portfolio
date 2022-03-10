09e11a1a056e1a8c9de096ce347d0abb53e3980a
devmailar <83369389+devmailar@users.noreply.github.com>
2022-03-10 18:08:59 +0200

    update

---
+++ assets/js/old.js
@@ -0,0 +1,51 @@
+function refreshPage() {
+  window.location.reload();
+}
+
+function sendMessage() {
+  const request = new XMLHttpRequest();
+  let name = document.querySelector(".name").value;
+  let email = document.querySelector(".email").value;
+  let subject = document.querySelector(".subject").value;
+  let message = document.querySelector(".message").value;
+  let currentDate = new Date();
+  let time = currentDate.getHours() + ":" + currentDate.getMinutes();
+  let webhookName = "Mailar";
+  let color = "#ff0000";
+
+  if (message.length < 1 || name.length < 1 || subject.length < 1 || email.length < 1) {
+    alert("Textbox is empty!");
+  } else if (message.length > 300 || name.length > 20 || subject.length > 20 || email.length > 20) {
+    alert(` 
+      Error on sending message!
+      message is too long!
+      characters: ${message.length}`);
+  } else {
+
+    request.open("POST", "https://discord.com/api/webhooks/942399439362990090/nL_f2NZr1td3y_AMzww-D467TbaEB2WPuD72GjUAo5ilIClR588QEvyuGIiI_V35O9oQ");
+    request.setRequestHeader("Content-Type", "application/json");
+
+    let myEmbed = {
+      author: {
+        name: "New Contact Form Submission",
+      },
+
+      title: "Subject: " + subject,
+      description:
+        "Name: " + name + "\nEmail: " + email + "\nSubject: " + subject + "\nMessage: " + message + "\n\nSended at **" + time + "** \n" + currentDate,
+      color: hexToDecimal(color),
+    };
+
+    const params = {
+      username: webhookName,
+      avatar_url: "",
+      embeds: [myEmbed],
+    };
+
+    request.send(JSON.stringify(params));
+
+    function hexToDecimal(hex) {
+      return parseInt(hex.replace("#", ""), 16);
+    }
+  }
+}
--- assets/js/post.js
+++ assets/js/post.js
@@ -1,36 +1,13 @@
 function sendMessage() {
-  let name = document.querySelector(".name").value;
-  let email = document.querySelector(".email").value;
-  let subject = document.querySelector(".subject").value;
-  let message = document.querySelector(".message").value;
-  // let currentDate = new Date();
-  // let time = currentDate.getHours() + ":" + currentDate.getMinutes();
-  let webhookName = "Mailar";
-  let color = "#ff0000";
-
   const request = new XMLHttpRequest();
   request.open("POST", "https://discord.com/api/webhooks/951506034957123614/gO6PRpMBDnSAhZOzm-04cKr7iA7mMHBcpPsG_iqF8QOjLLPS8PdOxTJn04BW5vv34082");
-  request.setRequestHeader('Content-type', 'application/json');
-
-/*   let myEmbed = {
-    author: {
-      name: "New Contact Form Submission",
-    },
-
-    title: "Subject: " + subject,
-    description:
-      "Name: " + name + "\nEmail: " + email + "\nSubject: " + subject + "\nMessage: " + message, color: hexToDecimal(color),
-  }; */
+  request.setRequestHeader("Content-type", "application/json");
 
   const params = {
-    username: webhookName,
+    username: "Webhook test",
     avatar_url: "",
-    content: "hello"
+    content: "hey sir!",
   };
 
   request.send(JSON.stringify(params));
-
-  function hexToDecimal(hex) {
-    return parseInt(hex.replace("#", ""), 16);
-  }
 }
