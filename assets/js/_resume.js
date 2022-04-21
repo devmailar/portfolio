ec3f2a73bbd74b4dae7a6712a09fdef8a916a9bc
mailar <veex1233@gmail.com>
2022-04-21 22:15:21 +0300

    update the portfolio to new release

--- assets/js/_resume.js
+++ assets/js/_resume.js
@@ -1,3 +1,6 @@
+let openResumeBtn = document.getElementById("resume");
+openResumeBtn.addEventListener("click", openResume);
+
 function openResume() {
   let resumeContainer = document.getElementById("resume-container");
   let main = document.querySelector("main");
